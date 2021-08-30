9d0c02300ab38682eda01a907cbbbeda6aa84f1a
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-30 19:57:34 +0530

    created AddGoals with vanilla js

---
+++ 01-starting-project/app.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const list = document.querySelector("ul");
+
+function addGoals() {
+  const enteredValue = input.value;
+  const listItem = document.createElement("li");
+  listItem.textContent = enteredValue;
+  list.appendChild(listItem);
+  input.value = " ";
+}
+button.addEventListener("click", addGoals);
