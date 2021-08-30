355ca4fe735d4f1ee495d31011cb118208f462cb
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-30 22:50:03 +0530

    munt.app

--- 01-starting-project/app.js
+++ 01-starting-project/app.js
@@ -1,3 +1,19 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app');
+
+
 // const button = document.querySelector("button");
 // const input = document.querySelector("input");
 // const list = document.querySelector("ul");
@@ -11,20 +27,3 @@
 // }
 // button.addEventListener("click", addGoals);
 
-// creating vue app
-Vue.createApp({
-  //configure
-  //pices of data view is aware of:
-  data() {
-    return {
-      goals: [],
-      enteredValue: "",
-    };
-  },
-  //this is functions
-  methods: {
-    addGoals() {
-      this.goals.push(this.enteredValue);
-    },
-  },
-});
