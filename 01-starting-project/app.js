fdba99e45b7bb7741b93f6c984efeeb9f00c92dc
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-30 20:44:05 +0530

    creating vue app

--- 01-starting-project/app.js
+++ 01-starting-project/app.js
@@ -1,12 +1,17 @@
-const button = document.querySelector("button");
-const input = document.querySelector("input");
-const list = document.querySelector("ul");
+// const button = document.querySelector("button");
+// const input = document.querySelector("input");
+// const list = document.querySelector("ul");
 
-function addGoals() {
-  const enteredValue = input.value;
-  const listItem = document.createElement("li");
-  listItem.textContent = enteredValue;
-  list.appendChild(listItem);
-  input.value = " ";
-}
-button.addEventListener("click", addGoals);
+// function addGoals() {
+//   const enteredValue = input.value;
+//   const listItem = document.createElement("li");
+//   listItem.textContent = enteredValue;
+//   list.appendChild(listItem);
+//   input.value = " ";
+// }
+// button.addEventListener("click", addGoals);
+
+// creating vue app
+Vue.createApp({
+  //configure
+});
